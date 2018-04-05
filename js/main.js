7975faad6f3781bdbd2ab5aa3094f8e44e2c5fa0
dawinn <dawinn@ya.ru>
2018-04-06 00:26:45 +0700

    Refactoring code, made class ScreenController

--- js/main.js
+++ js/main.js
@@ -1,62 +1,47 @@
 (function () {
   const keyCode = {
-    ALT: 18,
     RIGHT_ARROW: 39,
     LEFT_ARROW: 37
   };
-  const templateLinks = [];
-  let currentScreen = 0;
 
-  const templates = document.querySelectorAll(`template`);
-  templateLinks[0] = document.querySelector(`main.central`);
-  for (let i = 0; i < templates.length; i++) {
-    templateLinks[i + 1] = templates[i];
-  }
+  const ScreenController = {
+    currentScreen: 0,
+    templates: [],
 
-  const removeChildren = function (elem) {
-    while (elem.lastChild) {
-      elem.removeChild(elem.lastChild);
+    constructor() {
+      this.templates = document.querySelectorAll(`template`);
+    },
+
+    show() {
+      document.getElementsByClassName(`central`)[0].innerHTML = this.templates[this.currentScreen].innerHTML;
+    },
+
+    getNext() {
+      if (this.currentScreen > 0) {
+        this.currentScreen--;
+        this.show();
+      }
+    },
+
+    getPrev() {
+      if (this.currentScreen < this.templates.length - 1) {
+        this.currentScreen++;
+        this.show();
+      }
     }
   };
 
-  const showScreen = function (num) {
-    let mainScreen = templateLinks[0];
-    removeChildren(mainScreen);
-    mainScreen.appendChild(templateLinks[num].content.cloneNode(true));
-  };
-
-  const shiftScreen = function (step) {
-    currentScreen += step;
-    currentScreen = (step < 0 ? Math.max(currentScreen, 1) : currentScreen);
-    currentScreen = (step > 0 ? Math.min(currentScreen, templateLinks.length - 1) : currentScreen);
-    showScreen(currentScreen);
-  };
-
-  const pressedKeys = {};
-
-  const pressedAltArrow = function (keys) {
-    if (keys[keyCode.LEFT_ARROW]) {
-      shiftScreen(-1);
+  ScreenController.constructor();
+  ScreenController.show();
+  document.addEventListener(`keydown`, (evt) => {
+    if (evt.altKey) {
+      if (evt.keyCode === keyCode.LEFT_ARROW) {
+        ScreenController.getNext();
+      }
+      if (evt.keyCode === keyCode.RIGHT_ARROW) {
+        ScreenController.getPrev();
+      }
     }
-    if (keys[keyCode.RIGHT_ARROW]) {
-      shiftScreen(1);
-    }
-    showScreen(currentScreen);
-  };
-
-  const onKeyDown = function (evtDown) {
-    pressedKeys[evtDown.keyCode] = true;
-    if (!pressedKeys[keyCode.ALT] || !(pressedKeys[keyCode.LEFT_ARROW] || pressedKeys[keyCode.RIGHT_ARROW])) {
-      return;
-    }
-
-    pressedAltArrow(pressedKeys);
-  };
-
-  showScreen(currentScreen + 1);
-  document.addEventListener(`keydown`, onKeyDown);
-
-  document.addEventListener(`keyup`, function (evtUp) {
-    delete pressedKeys[evtUp.keyCode];
   });
+
 })();
