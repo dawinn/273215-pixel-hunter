9151c90be5d438b7ce7e49f3c2bcb75dc1a16d03
dawinn <dawinn@ya.ru>
2018-04-08 14:16:54 +0700

    module sharing

--- js/main.js
+++ js/main.js
@@ -1,47 +1,7 @@
-(function () {
-  const keyCode = {
-    RIGHT_ARROW: 39,
-    LEFT_ARROW: 37
-  };
+import {showScreen} from './utils.js';
+import greeting from './greeting.js';
 
-  const ScreenController = {
-    currentScreen: 0,
-    templates: [],
-
-    constructor() {
-      this.templates = document.querySelectorAll(`template`);
-    },
-
-    show() {
-      document.getElementsByClassName(`central`)[0].innerHTML = this.templates[this.currentScreen].innerHTML;
-    },
-
-    getNext() {
-      if (this.currentScreen > 0) {
-        this.currentScreen--;
-        this.show();
-      }
-    },
-
-    getPrev() {
-      if (this.currentScreen < this.templates.length - 1) {
-        this.currentScreen++;
-        this.show();
-      }
-    }
-  };
-
-  ScreenController.constructor();
-  ScreenController.show();
-  document.addEventListener(`keydown`, (evt) => {
-    if (evt.altKey) {
-      if (evt.keyCode === keyCode.LEFT_ARROW) {
-        ScreenController.getNext();
-      }
-      if (evt.keyCode === keyCode.RIGHT_ARROW) {
-        ScreenController.getPrev();
-      }
-    }
-  });
-
-})();
+const asteriskLink = document.querySelector(`.intro__asterisk`);
+asteriskLink.addEventListener(`click`, () => {
+  showScreen(greeting);
+});
